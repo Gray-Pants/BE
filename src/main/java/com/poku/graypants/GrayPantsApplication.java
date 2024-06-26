8ba49d861c20707b9e213769ca02eb3da27aaa72
jun <yoseb95@gmail.com>
2024-06-26 16:43:02 +0900

    feat: 초기 설정 Spring Boot: 3.3.1 dependencies: web, security, devtools, jpa, oauth2-client, lombok, h2

---
+++ src/main/java/com/poku/graypants/GrayPantsApplication.java
@@ -0,0 +1,13 @@
+package com.poku.graypants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrayPantsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrayPantsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/poku/graypants/GrayPantsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poku.graypants;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrayPantsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
