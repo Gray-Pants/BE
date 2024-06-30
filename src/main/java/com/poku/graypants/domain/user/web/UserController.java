ced7e0be1c7fb61f936722224fdbaf8eded164b4
POKUDING <yoseb95@gmail.com>
2024-07-01 08:50:17 +0900

    feat: 시큐리티 테스트용 api 구현

---
+++ src/main/java/com/poku/graypants/domain/user/web/UserController.java
@@ -0,0 +1,15 @@
+package com.poku.graypants.domain.user.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping("/me")
+    public String me() {
+        return "me";
+    }
+}
