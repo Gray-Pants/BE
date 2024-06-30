aa432954c8610894ab709eaf3cbff39cbbea2891
POKUDING <yoseb95@gmail.com>
2024-07-01 08:51:46 +0900

    feat: 시큐리티 설정파일 생성 및 설정 진행 중 #2

---
+++ src/main/java/com/poku/graypants/global/config/oauth/OAuthSecurityConfig.java
@@ -0,0 +1,25 @@
+package com.poku.graypants.global.config.oauth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class OAuthSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .logout(AbstractHttpConfigurer::disable)
+                .sessionManagement(management-> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+//                .addFilterBefore(필터 클래스, UsernamePasswordAuthenticationFilter.class)
+        return http.build();
+    }
+}
