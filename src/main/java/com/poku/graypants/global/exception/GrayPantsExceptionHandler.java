4bb25212236e14533197ba808fec456996e19714
jun <yoseb95@gmail.com>
2024-07-01 16:51:45 +0900

    fix: ExceptionHandler response 수정 임시로 null을 리턴하던 코드를 ApiResult를 반환하도록 수정 resolve : #3

--- src/main/java/com/poku/graypants/global/exception/GrayPantsExceptionHandler.java
+++ src/main/java/com/poku/graypants/global/exception/GrayPantsExceptionHandler.java
@@ -1,5 +1,9 @@
 package com.poku.graypants.global.exception;
 
+import static com.poku.graypants.global.util.ApiResponseUtil.error;
+
+import com.poku.graypants.global.util.ApiResponseUtil.ApiError;
+import com.poku.graypants.global.util.ApiResponseUtil.ApiResult;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
@@ -10,10 +14,9 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @Slf4j
 public class GrayPantsExceptionHandler {
     @ExceptionHandler(GrayPantsException.class)
-    public ResponseEntity<Object> handleVolumeTestException(GrayPantsException e) {
+    public ResponseEntity<ApiResult<ApiError>> handleVolumeTestException(GrayPantsException e) {
         //로그 로직 필요
         log.info("예외 발생 : {}", e.getMessage());
-//        return new ResponseEntity(error(e.getMessage()), new HttpHeaders(), e.getStatus());
-        return null;
+        return new ResponseEntity<>(error(e.getMessage()), new HttpHeaders(), e.getStatus());
     }
 }
