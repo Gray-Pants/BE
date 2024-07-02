1ca3d60cb7aa687094b8f467ac794e827ae1457e
jun <yoseb95@gmail.com>
2024-07-02 16:22:35 +0900

    feat: User 엔티티, 레포지토리, 서비스, 컨트롤러 생성 User객체와 OAuth2 객체가 합쳐져 있어서 분리 필요 related to : #2

---
+++ src/main/java/com/poku/graypants/domain/user/application/UserService.java
@@ -0,0 +1,7 @@
+package com.poku.graypants.domain.user.application;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+}
--- src/main/java/com/poku/graypants/domain/user/persistence/User.java
+++ src/main/java/com/poku/graypants/domain/user/persistence/User.java
@@ -33,11 +33,30 @@ public class User {
     @Column(name="grade", length=50)
     private String grade;
 
+    @Column(name="refresh_token")
+    private String refreshToken;
+
+    //분리 필요
+    @Column(name = "oauth2_id", length = 100, unique = true)
+    private String oauth2Id;
+
+    @Column(name = "provider", length = 100)
+    private String provider;
+
+    @Column(name = "provider_id", length = 100, unique = true)
+    private String providerId;
+
+    public void updateRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
     @Builder
-    public User(String username, String email, String password, String grade) {
+    public User(String username, String email, String password, String grade, String oauth2Id, String provider, String providerId) {
         this.username = username;
         this.email = email;
         this.password = password;
         this.grade = grade;
+        this.oauth2Id = oauth2Id;
+        this.provider = provider;
+        this.providerId = providerId;
     }
 }
