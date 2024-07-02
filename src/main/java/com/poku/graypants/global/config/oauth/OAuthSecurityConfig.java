0a9ca0ba9dbeeb29fbc009753213705260422dde
jun <yoseb95@gmail.com>
2024-07-02 16:35:09 +0900

    feat: SecurityConfig 생성 jwt 인증필터 및 oauth2를 이용한 인증로직 삽입 related to : #2

--- src/main/java/com/poku/graypants/global/config/oauth/OAuthSecurityConfig.java
+++ src/main/java/com/poku/graypants/global/config/oauth/OAuthSecurityConfig.java
@@ -1,16 +1,29 @@
 package com.poku.graypants.global.config.oauth;
 
+import com.poku.graypants.domain.user.application.UserService;
+import com.poku.graypants.domain.user.persistence.UserRepository;
+import com.poku.graypants.global.config.filter.TokenAuthenticationFilter;
+import com.poku.graypants.global.jwt.JwtProvider;
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+@AllArgsConstructor
 @Configuration
 public class OAuthSecurityConfig {
 
+    private final OAuth2UserCustomService oAuth2UserCustomService;
+    private final UserRepository userRepository;
+    private final JwtProvider tokenProvider;
+    private final UserService userService;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -18,8 +31,40 @@ public class OAuthSecurityConfig {
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
                 .logout(AbstractHttpConfigurer::disable)
-                .sessionManagement(management-> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-//                .addFilterBefore(필터 클래스, UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement(management-> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(new TokenAuthenticationFilter(tokenProvider), UsernamePasswordAuthenticationFilter.class)
+                .authorizeHttpRequests((auth)-> auth
+                        .requestMatchers(new AntPathRequestMatcher("/api/token")).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/api/**")).authenticated()
+                        .anyRequest().permitAll())
+                .oauth2Login(oauth2 -> oauth2
+                        .authorizationEndpoint(
+                                authorizationEndpoint -> authorizationEndpoint.authorizationRequestRepository(
+                                        oAuth2AuthorizationRequestBasedOnCookieRepository()))
+                        .userInfoEndpoint(userInfoEndpoint -> userInfoEndpoint.userService(oAuth2UserCustomService))
+                        .successHandler(oAuth2SuccessHandler())
+                )
+                .exceptionHandling(exceptionHandling -> exceptionHandling
+                        .defaultAuthenticationEntryPointFor(
+                                new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED),
+                                new AntPathRequestMatcher("/api/**"))
+                );
         return http.build();
     }
+
+    @Bean
+    public OAuth2AuthorizationRequestBasedOnCookieRepository oAuth2AuthorizationRequestBasedOnCookieRepository() {
+        return new OAuth2AuthorizationRequestBasedOnCookieRepository();
+    }
+
+    @Bean
+    public OAuth2SuccessHandler oAuth2SuccessHandler() {
+        return new OAuth2SuccessHandler(tokenProvider,
+                userRepository,
+                oAuth2AuthorizationRequestBasedOnCookieRepository(),
+                userService
+        );
+    }
+
+
 }
