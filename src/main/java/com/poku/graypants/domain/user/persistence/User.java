e8f6e190c634162d03a501ff55abc72026b27bcb
jun <yoseb95@gmail.com>
2024-07-03 14:36:26 +0900

    feat: User 테이블에서 OAuth 관련 정보 삭제 related to : #2

--- src/main/java/com/poku/graypants/domain/user/persistence/User.java
+++ src/main/java/com/poku/graypants/domain/user/persistence/User.java
@@ -36,27 +36,14 @@ public class User {
     @Column(name="refresh_token")
     private String refreshToken;
 
-    //분리 필요
-    @Column(name = "oauth2_id", length = 100, unique = true)
-    private String oauth2Id;
-
-    @Column(name = "provider", length = 100)
-    private String provider;
-
-    @Column(name = "provider_id", length = 100, unique = true)
-    private String providerId;
-
     public void updateRefreshToken(String refreshToken) {
         this.refreshToken = refreshToken;
     }
     @Builder
-    public User(String username, String email, String password, String grade, String oauth2Id, String provider, String providerId) {
+    public User(String username, String email, String password, String grade) {
         this.username = username;
         this.email = email;
         this.password = password;
         this.grade = grade;
-        this.oauth2Id = oauth2Id;
-        this.provider = provider;
-        this.providerId = providerId;
     }
 }
