ed50901d3f446ee64c910d8f47c4577b792c739a
jun <yoseb95@gmail.com>
2024-07-03 19:22:06 +0900

    feat: 이메일 인증 정보 비즈니스 로직 구현 redis 로 정보 저장 로직 추가 필요 RelatedTo : #12

--- src/main/java/com/poku/graypants/domain/user/application/UserService.java
+++ src/main/java/com/poku/graypants/domain/user/application/UserService.java
@@ -1,16 +1,23 @@
 package com.poku.graypants.domain.user.application;
 
+import com.poku.graypants.domain.mail.application.MailService;
 import com.poku.graypants.domain.user.persistence.User;
 import com.poku.graypants.domain.user.persistence.UserRepository;
 import com.poku.graypants.global.config.oauth.info.OAuth2UserInfo;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Random;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final MailService mailService;
     private final String DEFAULT_ROLE = "ROLE_DEFAULT";
+    private String authCode;
 
     public User saveOrGetUserByOAuth2Info(OAuth2UserInfo oAuth2UserInfo) {
         return userRepository.findByEmail(oAuth2UserInfo.getEmail())
@@ -23,4 +30,35 @@ public class UserService {
                 .username(username)
                 .build());
     }
+
+    public void sendCodeToEmail(String email) {
+        String title = "Gray-Pants 이메일 인증 번호";
+        authCode = this.createCode();
+        mailService.sendMail(email, title, authCode);
+        // 이메일 인증 요청 시 인증 번호 Redis에 저장 ( key = "AuthCode " + Email / value = AuthCode )
+//        redisService.setValues(AUTH_CODE_PREFIX + toEmail,
+//                authCode, Duration.ofMillis(this.authCodeExpirationMillis));
+
+    }
+
+    private String createCode() {
+        int length = 6;
+        try {
+            Random random = SecureRandom.getInstanceStrong();
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < length; i++) {
+                builder.append(random.nextInt(10));
+            }
+            return builder.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public boolean verifiedCode(String email, String code) {
+        return authCode.equals(code);
+    }
+
+
 }
