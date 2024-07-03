e2034ed32ac93c2b9206a2a771d71f93ec0e7ba9
jun <yoseb95@gmail.com>
2024-07-03 14:32:50 +0900

    refactor: OAuth2UserInfo 를 이용하여 User 를 가져오는 로직 변경 OAuthService 로 책임 위임 related to : #2

--- src/main/java/com/poku/graypants/global/config/oauth/OAuth2UserCustomService.java
+++ src/main/java/com/poku/graypants/global/config/oauth/OAuth2UserCustomService.java
@@ -1,10 +1,10 @@
 package com.poku.graypants.global.config.oauth;
 
+import com.poku.graypants.domain.application.OAuthService;
+import com.poku.graypants.domain.auth.persistence.OAuth;
 import com.poku.graypants.domain.user.persistence.User;
-import com.poku.graypants.domain.user.persistence.UserRepository;
 import com.poku.graypants.global.config.oauth.info.OAuth2UserInfo;
 import com.poku.graypants.global.config.oauth.info.OAuth2UserInfoFactory;
-import java.util.Optional;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
@@ -21,7 +21,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 @Service
 public class OAuth2UserCustomService extends DefaultOAuth2UserService {
-    private final UserRepository userRepository;
+    private final OAuthService oAuthService;
 
     /**
      * OAuth2UserRequest 를 받아서 UserRepository 에 저장 혹은 닉네임 업데이트 후 OAuth2User 를 반환하는 메소드
@@ -46,20 +46,8 @@ public class OAuth2UserCustomService extends DefaultOAuth2UserService {
      * @return User 객체
      */
     private User saveOrGet(final OAuth2UserInfo oAuth2UserInfo) {
+        OAuth oAuth = oAuthService.saveOrGetOAuthByOAuth2UserInfo(oAuth2UserInfo);
 
-        String oauth2Id = oAuth2UserInfo.getProvider() + "_"
-                + oAuth2UserInfo.getProviderId(); //중복이 발생하지 않도록 provider 와 providerId를 조합
-        String role = "ROLE_DEFAULT"; //일반 유저
-        Optional<User> user = userRepository.findByOauth2Id(oauth2Id);
-        return user.orElseGet(() -> userRepository.save(User.builder()
-                .oauth2Id(oauth2Id)
-                .provider(oAuth2UserInfo.getProvider())
-                .providerId(oAuth2UserInfo.getProviderId())
-                .grade(role)
-                .email(oAuth2UserInfo.getEmail())
-                .username(oAuth2UserInfo.getName())
-                .build()));
+        return oAuth.getUser();
     }
-
-
 }
