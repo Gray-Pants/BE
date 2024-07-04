4b017e5fd9d299d4f12e0fceb5b8ca25be0429ab
jun <yoseb95@gmail.com>
2024-07-04 18:42:22 +0900

    refactor: 이메일 인증로직 이동 UserService 에서 MailService 로 이동 RelatedTo : #12

--- src/main/java/com/poku/graypants/domain/mail/application/MailService.java
+++ src/main/java/com/poku/graypants/domain/mail/application/MailService.java
@@ -1,6 +1,14 @@
 package com.poku.graypants.domain.mail.application;
 
+import com.poku.graypants.global.exception.ExceptionStatus;
+import com.poku.graypants.global.exception.GrayPantsException;
+import jakarta.annotation.Resource;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.time.Duration;
+import java.util.Random;
 import lombok.AllArgsConstructor;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -10,6 +18,9 @@ import org.springframework.stereotype.Service;
 public class MailService {
 
     private final JavaMailSender javaMailSender;
+
+    @Resource(name = "redisTemplate")
+    private ValueOperations<String, String> valueOperations;
 
     public void sendMail(String to, String subject, String text) {
         SimpleMailMessage message = createMailMessage(to, subject, text);
@@ -24,4 +35,33 @@ public class MailService {
         return message;
     }
 
+    public void sendCodeToEmail(String email) {
+        String title = "Gray-Pants 이메일 인증 번호";
+        String authCode = this.createCode();
+        this.sendMail(email, title, authCode);
+        valueOperations.set("AuthCode " + email, authCode, Duration.ofMinutes(10));
+    }
+
+    private String createCode() {
+        int length = 6;
+        Random random = null;
+        try {
+            random = SecureRandom.getInstanceStrong();
+        } catch (NoSuchAlgorithmException e) {
+            throw new GrayPantsException(ExceptionStatus.RANDOM_NUMBER_GENERATE_FAILED);
+        }
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            builder.append(random.nextInt(10));
+        }
+        return builder.toString();
+    }
+
+    public boolean verifiedCode(String email, String code) {
+        String authCode = valueOperations.get("AuthCode " + email);
+        if(!code.equals(authCode))
+            return false;
+        valueOperations.getAndDelete("AuthCode " + email);
+        return true;
+    }
 }
--- src/main/java/com/poku/graypants/domain/user/application/UserService.java
+++ src/main/java/com/poku/graypants/domain/user/application/UserService.java
@@ -22,8 +22,6 @@ public class UserService {
     private final MailService mailService;
     private final String DEFAULT_ROLE = "ROLE_DEFAULT";
 
-    @Resource(name = "redisTemplate")
-    private ValueOperations<String, String> valueOperations;
 
     public User saveOrGetUserByOAuth2Info(OAuth2UserInfo oAuth2UserInfo) {
         return userRepository.findByEmail(oAuth2UserInfo.getEmail())
@@ -37,36 +35,5 @@ public class UserService {
                 .build());
     }
 
-    public void sendCodeToEmail(String email) {
-        String title = "Gray-Pants 이메일 인증 번호";
-        String authCode = this.createCode();
-        mailService.sendMail(email, title, authCode);
-        // 이메일 인증 요청 시 인증 번호 Redis에 저장 ( key = "AuthCode " + Email / value = AuthCode )
-//        redisService.setValues(AUTH_CODE_PREFIX + toEmail,
-//                authCode, Duration.ofMillis(this.authCodeExpirationMillis));
-        valueOperations.set("AuthCode " + email, authCode, Duration.ofMinutes(10));
-    }
 
-    private String createCode() {
-        int length = 6;
-        Random random = null;
-        try {
-            random = SecureRandom.getInstanceStrong();
-        } catch (NoSuchAlgorithmException e) {
-            throw new GrayPantsException(ExceptionStatus.RANDOM_NUMBER_GENERATE_FAILED);
-        }
-        StringBuilder builder = new StringBuilder();
-        for (int i = 0; i < length; i++) {
-            builder.append(random.nextInt(10));
-        }
-        return builder.toString();
-    }
-
-    public boolean verifiedCode(String email, String code) {
-        String authCode = valueOperations.get("AuthCode " + email);
-        if(!code.equals(authCode))
-            return false;
-        valueOperations.getAndDelete("AuthCode " + email);
-        return true;
-    }
 }
