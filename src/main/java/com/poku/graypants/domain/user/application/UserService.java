e8d104b09fe86912ff0e63a415b8c3e8a3ebba0f
jun <yoseb95@gmail.com>
2024-07-04 16:54:22 +0900

    feat: sendCodeToEmail, verifiedCode 완성 레디스를 이용하여 6개 숫자를 저장하는 로직으로 변경 RelatedTo : #12

--- src/main/java/com/poku/graypants/domain/user/application/UserService.java
+++ src/main/java/com/poku/graypants/domain/user/application/UserService.java
@@ -4,11 +4,15 @@ import com.poku.graypants.domain.mail.application.MailService;
 import com.poku.graypants.domain.user.persistence.User;
 import com.poku.graypants.domain.user.persistence.UserRepository;
 import com.poku.graypants.global.config.oauth.info.OAuth2UserInfo;
+import com.poku.graypants.global.exception.ExceptionStatus;
+import com.poku.graypants.global.exception.GrayPantsException;
+import jakarta.annotation.Resource;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.time.Duration;
 import java.util.Random;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,7 +21,9 @@ public class UserService {
     private final UserRepository userRepository;
     private final MailService mailService;
     private final String DEFAULT_ROLE = "ROLE_DEFAULT";
-    private String authCode;
+
+    @Resource(name = "redisTemplate")
+    private ValueOperations<String, String> valueOperations;
 
     public User saveOrGetUserByOAuth2Info(OAuth2UserInfo oAuth2UserInfo) {
         return userRepository.findByEmail(oAuth2UserInfo.getEmail())
@@ -33,32 +39,34 @@ public class UserService {
 
     public void sendCodeToEmail(String email) {
         String title = "Gray-Pants 이메일 인증 번호";
-        authCode = this.createCode();
+        String authCode = this.createCode();
         mailService.sendMail(email, title, authCode);
         // 이메일 인증 요청 시 인증 번호 Redis에 저장 ( key = "AuthCode " + Email / value = AuthCode )
 //        redisService.setValues(AUTH_CODE_PREFIX + toEmail,
 //                authCode, Duration.ofMillis(this.authCodeExpirationMillis));
-
+        valueOperations.set("AuthCode " + email, authCode, Duration.ofMinutes(10));
     }
 
     private String createCode() {
         int length = 6;
+        Random random = null;
         try {
-            Random random = SecureRandom.getInstanceStrong();
-            StringBuilder builder = new StringBuilder();
-            for (int i = 0; i < length; i++) {
-                builder.append(random.nextInt(10));
-            }
-            return builder.toString();
+            random = SecureRandom.getInstanceStrong();
         } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-            return null;
+            throw new GrayPantsException(ExceptionStatus.RANDOM_NUMBER_GENERATE_FAILED);
         }
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            builder.append(random.nextInt(10));
+        }
+        return builder.toString();
     }
 
     public boolean verifiedCode(String email, String code) {
-        return authCode.equals(code);
+        String authCode = valueOperations.get("AuthCode " + email);
+        if(!code.equals(authCode))
+            return false;
+        valueOperations.getAndDelete("AuthCode " + email);
+        return true;
     }
-
-
 }
