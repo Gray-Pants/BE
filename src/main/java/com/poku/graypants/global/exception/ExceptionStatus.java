d49286bfca8ce41ed11dfed2706a9efee592c8b7
jun <yoseb95@gmail.com>
2024-07-08 14:48:43 +0900

    hotfix: enum 문법오류 수정

--- src/main/java/com/poku/graypants/global/exception/ExceptionStatus.java
+++ src/main/java/com/poku/graypants/global/exception/ExceptionStatus.java
@@ -10,7 +10,7 @@ public enum ExceptionStatus {
     ITEM_NOT_FOUND(HttpStatus.NOT_FOUND, 404, "Item Not Found"),
     Like_NOT_FOUND(HttpStatus.NOT_FOUND, 404, "Like Not Found"),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, 404,"User not found"),
-    INVALID_QUANTITY(HttpStatus.BAD_REQUEST, 400, "유효하지 않은 수량입니다." );
+    INVALID_QUANTITY(HttpStatus.BAD_REQUEST, 400, "유효하지 않은 수량입니다." ),
     CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, 404, "Category Not Found"),
     INVALID_VERIFICATION_CODE(HttpStatus.BAD_REQUEST, 400, "유효하지 않은 메일 인증입니다."),
     INVALID_PASSWORD(HttpStatus.FORBIDDEN, 400, "Invalid Password");
