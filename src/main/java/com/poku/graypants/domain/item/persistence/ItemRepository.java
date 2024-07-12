980d12fd49ee7c98942cd464ecb6dd6fe9af30e5
Jinseonghyeon <jinsunghyun9@gmail.com>
2024-07-12 15:07:34 +0900

    feat: 판매자 상품 목록 데이터연동 리포지터리 리스트타입 메서드 추가 relatedTo: #50

--- src/main/java/com/poku/graypants/domain/item/persistence/ItemRepository.java
+++ src/main/java/com/poku/graypants/domain/item/persistence/ItemRepository.java
@@ -3,9 +3,12 @@ package com.poku.graypants.domain.item.persistence;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
     Optional<Item> findById(Long id);
+
+    List<Item> findByStore_StoreId(Long storeId);
 }
