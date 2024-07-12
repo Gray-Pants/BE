5d747c97782424c998adee90abe12b06e2e78a6c
jun <yoseb95@gmail.com>
2024-07-12 13:18:20 +0900

    feat: 찜한 상품 목록 가져오는 API 개발

--- src/main/java/com/poku/graypants/domain/like/application/dto/LikeResponseDto.java
+++ src/main/java/com/poku/graypants/domain/like/application/dto/LikeResponseDto.java
@@ -1,5 +1,6 @@
 package com.poku.graypants.domain.like.application.dto;
 
+import com.poku.graypants.domain.item.application.dto.ItemResponseDto;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,11 +13,12 @@ public class LikeResponseDto {
     private Long likeId;
     private Long userId;
     private Long itemId;
+    private ItemResponseDto item;
 
     @Builder
-    public LikeResponseDto(Long likeId, Long userId, Long itemId) {
+    public LikeResponseDto(Long likeId, Long userId, ItemResponseDto item) {
         this.likeId = likeId;
         this.userId = userId;
-        this.itemId = itemId;
+        this.item = item;
     }
 }
