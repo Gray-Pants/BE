14e8e9a6bb638ffe7e4f45f78245b034e8f92e55
POKUDING <yoseb95@gmail.com>
2024-07-13 01:43:51 +0900

    feat: 내 리뷰 조회 API 구현 relatedTo: #49

--- src/main/java/com/poku/graypants/domain/user/web/UserController.java
+++ src/main/java/com/poku/graypants/domain/user/web/UserController.java
@@ -2,6 +2,7 @@ package com.poku.graypants.domain.user.web;
 
 import static com.poku.graypants.global.util.ApiResponseUtil.success;
 
+import com.poku.graypants.domain.review.application.dto.ReviewResponseDTO;
 import com.poku.graypants.domain.user.application.UserService;
 import com.poku.graypants.domain.user.application.dto.MyProfileResponseDto;
 import com.poku.graypants.global.util.ApiResponseUtil.ApiResult;
@@ -13,6 +14,8 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
@@ -28,5 +31,11 @@ public class UserController {
         return new ResponseEntity<>(success(response), HttpStatus.OK);
     }
 
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @GetMapping("/reviews")
+    public ResponseEntity<ApiResult<List<ReviewResponseDTO>>> reviews(Authentication authentication) {
+        List<ReviewResponseDTO> response = userService.getMyReviews((Long)authentication.getPrincipal());
+        return new ResponseEntity<>(success(response), HttpStatus.OK);
+    }
 
 }
