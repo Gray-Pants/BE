318fd24f70bab26cf0eb6d7477d2d5d90eca2509
jun <yoseb95@gmail.com>
2024-07-13 18:38:53 +0900

    feat: passwordEncoder 추가 relatedTo: #57

--- src/main/java/com/poku/graypants/domain/user/application/PasswordService.java
+++ src/main/java/com/poku/graypants/domain/user/application/PasswordService.java
@@ -3,18 +3,18 @@ package com.poku.graypants.domain.user.application;
 import com.poku.graypants.domain.user.application.dto.PasswordEditRequestDto;
 import com.poku.graypants.domain.user.persistence.User;
 import com.poku.graypants.domain.user.persistence.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 public class PasswordService {
 
-    @Autowired
     private UserRepository userRepository;
 
-    @Autowired
     private PasswordEncoder passwordEncoder;
 
     @Transactional
