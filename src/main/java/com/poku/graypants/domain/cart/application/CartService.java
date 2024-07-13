d29a8275a3bc5c901ce42f082eb8929d8be44271
jun <yoseb95@gmail.com>
2024-07-13 11:42:24 +0900

    refactor: Cart CRUD 재정의 relatedTo: #49

--- src/main/java/com/poku/graypants/domain/cart/application/CartService.java
+++ src/main/java/com/poku/graypants/domain/cart/application/CartService.java
@@ -1,69 +1,50 @@
 package com.poku.graypants.domain.cart.application;
 
+import com.poku.graypants.domain.cart.application.dto.CartItemAddRequestDto;
+import com.poku.graypants.domain.cart.application.dto.CartItemResponseDto;
+import com.poku.graypants.domain.cart.application.dto.CartItemUpdateRequestDto;
 import com.poku.graypants.domain.cart.persistence.CartItem;
-import com.poku.graypants.domain.cart.persistence.CartItemRepository;
-import com.poku.graypants.domain.cart.persistence.CartRepository;
 import com.poku.graypants.domain.item.application.ItemService;
 import com.poku.graypants.domain.item.persistence.Item;
 import com.poku.graypants.domain.user.application.UserService;
-import com.poku.graypants.global.exception.ExceptionStatus;
-import com.poku.graypants.global.exception.GrayPantsException;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.Optional;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class CartService {
 
-    private final CartRepository cartRepository;
-    private final CartItemRepository cartItemRepository;
+    private final CartItemService cartItemService;
     private final UserService userService;
     private final ItemService itemService;
 
-    public CartItem addItemToCart(Long item_id, int quantity, String email) {
-        userService.getUserByEmail(email);
-        Item item = itemService.getItemById(item_id);
-//        CartItem cartItem =  CartItem.builder()
-//                .cart(myCart)
-//                .item(item)
-//                .cartItemQuantity(quantity)
-//                .build();
-//        return cartItemRepository.save(cartItem);
-        return null;
+
+    public List<CartItemResponseDto> getMyCartItems(Long principal) {
+        List<CartItem> cartItems = cartItemService.getCartItemsByUserId(principal);
+        return cartItems.stream().map(CartItemResponseDto::new).toList();
     }
 
-    @Transactional(readOnly = true)
-    public List<CartItem> getCartItems(String email) {
-//        Cart cart = userService.getCartByEmail(email);
-//        return cartItemRepository.findByCart(cart);
-        return null;
+    public void createCartItem(CartItemAddRequestDto request, Long UserId) {
+        Item item = itemService.getVerifyItemById(request.getItemId());
+        cartItemService.createCartItem(request.getQuantity(), userService.getVerifyUserByUserId(UserId), item);
     }
 
-    public void removeItemFromCart(Long cartItem_id, String email) {
-        Optional<CartItem> cartItem = cartItemRepository.findById(cartItem_id);
-        if (cartItem.isEmpty()) {
-            throw new GrayPantsException(ExceptionStatus.ITEM_NOT_FOUND);
-        }
-        if(cartItem.get().getCart().getUser().getEmail().equals(email)) {
-            cartItemRepository.deleteById(cartItem_id);
-        };
-    }
-
-    public void updateCartItemQuantity(Long cartItem_id, int quantity, String email) {
-        Optional<CartItem> cartItem = cartItemRepository.findById(cartItem_id);
-        if (cartItem.isEmpty()) {
-            throw new GrayPantsException(ExceptionStatus.ITEM_NOT_FOUND);
-        }
-        if(cartItem.get().getCart().getUser().getEmail().equals(email)) {
-            if (quantity <= 0) {
-                throw new GrayPantsException(ExceptionStatus.INVALID_QUANTITY);
+    public List<CartItemResponseDto> updateCartItem(CartItemUpdateRequestDto request, Long principal) {
+        List<CartItem> cartItems = cartItemService.getCartItemsByUserId(principal);
+        cartItems.forEach(cartItem -> {
+            if (cartItem.getCartItemId().equals(request.getCartItemId())) {
+                cartItem.updateQuantity(request.getNewQuantity());
             }
-            cartItem.get().updateQuantity(quantity);
-        };
+        });
+        return cartItems.stream().map(CartItemResponseDto::new).toList();
+    }
+
+    public List<CartItemResponseDto> deleteCartItem(Long cartItemId, Long principal) {
+        cartItemService.deleteCartItem(cartItemId, principal);
+        return getMyCartItems(principal);
     }
 }
+
