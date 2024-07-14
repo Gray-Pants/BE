1872ad638c1bd9ba34a675f6753f875e4cf7ad23
jun <yoseb95@gmail.com>
2024-07-14 14:40:26 +0900

    feat: 주문내역 페이지 데이터전송 로직

--- src/main/java/com/poku/graypants/domain/store/web/StoreController.java
+++ src/main/java/com/poku/graypants/domain/store/web/StoreController.java
@@ -1,6 +1,7 @@
 package com.poku.graypants.domain.store.web;
 
 import com.poku.graypants.domain.item.application.dto.ItemResponseDto;
+import com.poku.graypants.domain.store.application.MyOrderItemsResponseDto;
 import com.poku.graypants.domain.store.application.StoreService;
 import com.poku.graypants.global.util.ApiResponseUtil;
 import com.poku.graypants.global.util.ApiResponseUtil.ApiResult;
@@ -9,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,6 +33,13 @@ public class StoreController {
     @GetMapping("/myitems")
     public ResponseEntity<ApiResult<List<ItemResponseDto>>> getItemList(Authentication authentication) {
         List<ItemResponseDto> response = storeService.getItemByStoreId((Long) authentication.getPrincipal());
+        return new ResponseEntity<>(success(response), HttpStatus.OK);
+    }
+
+    @PreAuthorize("hasRole('ROLE_STORE')")
+    @GetMapping("/my-order-items")
+    public ResponseEntity<ApiResult<MyOrderItemsResponseDto>> getMyOrderItems(@AuthenticationPrincipal Long storeId) {
+        MyOrderItemsResponseDto response = storeService.getMyOrderItems(storeId);
         return new ResponseEntity<>(success(response), HttpStatus.OK);
     }
 }
